531e3b7ad597e943660264c1b7fb69e0313ceeb9
Brage Hogstad <bragehogstad@hotmail.com>
2023-04-24 14:36:16 +0200

    Byttet ut lambda funksjon i test pga ny klasse løste problemet

--- src/test/java/no/uib/inf101/sem2/twentyfourtyeight/model/TestGameOver.java
+++ src/test/java/no/uib/inf101/sem2/twentyfourtyeight/model/TestGameOver.java
@@ -1,6 +1,7 @@
 package no.uib.inf101.sem2.twentyfourtyeight.model;
 
 import no.uib.inf101.sem2.grid.CellPosition;
+import no.uib.inf101.sem2.twentyfourtyeight.tile.FixedTileFactory;
 import no.uib.inf101.sem2.twentyfourtyeight.tile.Tile;
 import no.uib.inf101.sem2.twentyfourtyeight.tile.TileFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,32 +9,32 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGameOver {
-    private GameModel gameModel;
-    private GameBoard gameBoard;
-    private TileFactory tileFactory;
+    private GameModel model;
+    private GameBoard board;
+    private TileFactory factory;
 
     @BeforeEach
     public void setUp() {
-        gameBoard = new GameBoard(4, 4);
-        tileFactory = (position) -> new Tile(2, position);
-        gameModel = new GameModel(gameBoard, tileFactory);
+        board = new GameBoard(4, 4);
+        factory = new FixedTileFactory(2);
+        model = new GameModel(board, factory);
     }
 
     @Test
     public void testIsGameOverFalse() {
-        assertFalse(gameModel.isGameOver());
+        assertFalse(model.isGameOver());
     }
 
     @Test
     public void testIsGameOverTrue() {
         // Fill the board with tiles in a way that no valid moves are possible
-        for (int row = 0; row < gameBoard.rows(); row++) {
-            for (int col = 0; col < gameBoard.cols(); col++) {
+        for (int row = 0; row < board.rows(); row++) {
+            for (int col = 0; col < board.cols(); col++) {
                 int value = (row + col) % 2 == 0 ? 2 : 4;
-                gameBoard.set(new CellPosition(row, col), value);
+                board.set(new CellPosition(row, col), value);
             }
         }
 
-        assertTrue(gameModel.isGameOver());
+        assertTrue(model.isGameOver());
     }
 }
